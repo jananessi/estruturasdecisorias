5a52660f1144ae0bc007f7278c16953d52247603
Janaina Nessi <83977818+jananessi@users.noreply.github.com>
2025-12-30 20:39:46 -0300

    Ifelse.c

--- ifelseelif.c
+++ ifelseelif.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int main() {
+    int idadevoto = 16;
+    int idade;
+    printf("digite a idade: ");
+    scanf("%d", &idade);
+    if (idade >= idadevoto)
+        printf("Pode votar");
+    else
+        printf("Não pode votar ainda");
+}    
